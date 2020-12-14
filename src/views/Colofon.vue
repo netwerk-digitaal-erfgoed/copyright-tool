e0da59766fe140f88fd90cd42eb1717a6357fca3
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2020-12-14 14:48:07 +0100

    Update colofon & disclaimer

--- src/views/Colofon.vue
+++ src/views/Colofon.vue
@@ -9,6 +9,8 @@
 
         <p>Het beslismodel is een hulpmiddel bij het uitzoeken en clearen van auteursrechten op werken in collecties. Aan de uitkomsten kunnen geen rechten ontleend worden en we willen de gebruikers er dan ook op wijzen dat ze zelf verantwoordelijk blijven voor het op een juiste manier regelen van de rechten.</p>
 
+        <p>Wanneer je onze website gebruikt, ga je akkoord met de voorwaarden zoals in de <a href="https://www.den.nl/over-ons/gebruikersvoorwaarden" target="_blank">gebruikersvoorwaarden</a> beschreven. Wij hechten waarde aan jouw privacy en gaan zorgvuldig om met je gegevens. In onze <a href="https://www.den.nl/privacy" target="_blank">verklaring</a> lees je welke gegevens we verwerken en met welk doel.</p>
+
         <h2>Feedback</h2>
 
         <p>
